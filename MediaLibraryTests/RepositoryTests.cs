51a9a60913fa5aae7ba87a1b879026bee3f30010
Joshua Sheffield <edgixshua@gmail.com>
2017-05-30 18:53:31 +0100

    Begun to add test coverage to solution
    Begun to add test coverage to solution. Using NUnit over MSTest. Build error presents when attempting to discover created test. Will investigate further.

--- MediaLibraryTests/RepositoryTests.cs
+++ MediaLibraryTests/RepositoryTests.cs
@@ -1,32 +1,42 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using EdgixshuaMediaLibrary.VideoGames;
 using EdgixshuaMediaLibrary.Database;
 using System.Collections.Generic;
 using System.Linq;
 using System.Data.Entity;
 using Shouldly;
+using NUnit.Framework;
+using EdgixshuaMediaLibrary.DataLayer;
+using AutoMapper;
 
 namespace MediaLibraryTests
 {
-    [TestClass]
+    [TestFixture]
     public class RepositoryTests
     {
+        public VideoGamesRespository Repository = new VideoGamesRespository();
         public MediaLibraryEntities Entities = new MediaLibraryEntities();
 
-        [TestMethod]
+        [Test]
         public void ReturnVideoGame()
         {
-            //Arrange
-            //var videoGame = new VideoGame();
-            //string gameTitle = "Rocket League";
+            // Arrange
+            Video_Games game = new Video_Games()
+            {
+                Title = "Rocket League",
+                Edition = "Standard",
+                Platform = "Playstation 4",
+                Year = 2014
+            };
 
-            //Act
-            //videoGame = Entities.Video_Games.FirstOrDefault();
+            // Act
+            Entities.Video_Games.Add(game);
+            Entities.SaveChanges();
 
-            //Assert
-            //videoGame.Title.ShouldBe(gameTitle);
+            var videoGame = Mapper.Map<List<VideoGame>>(Entities.Video_Games.FirstOrDefault());
 
+            // Assert
+            Assert.AreEqual("Rocket League", videoGame.Select(s => s.Title));
         }
     }
 }
