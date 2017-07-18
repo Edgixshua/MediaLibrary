0f9693001663aab1c4ac4b1760cb6aa8d7f51f66
Joshua Sheffield <edgixshua@gmail.com>
2017-07-18 09:34:07 +0100

    Added lambdas to repo

--- EdgixshuaMediaLibrary/DataLayer/VideoGamesRespository.cs
+++ EdgixshuaMediaLibrary/DataLayer/VideoGamesRespository.cs
@@ -22,24 +22,16 @@ namespace EdgixshuaMediaLibrary.DataLayer
         }
 
         private List<VideoGame> MapVideoGames(object value)
-        {
-            return Mapper.Map<List<VideoGame>>(value);
-        }
+            => Mapper.Map<List<VideoGame>>(value);
 
         public List<VideoGame> GetEntireVideoGameLibrary()
-        {
-            return MapVideoGames(Entities.Video_Games.ToList());
-        }
-
+            => MapVideoGames(Entities.Video_Games.ToList());
+        
         public List<VideoGame> SearchByPlatform(string platformName)
-        {
-            return MapVideoGames(Entities.Video_Games.Where(w => w.Platform == platformName).ToList());
-        }
+            => MapVideoGames(Entities.Video_Games.Where(w => w.Platform == platformName).ToList());
 
         public List<VideoGame> SearchByTitle(string title)
-        {
-            return MapVideoGames(Entities.Video_Games.Where(w => w.Title.Contains(title)).ToList());
-        }
+            => MapVideoGames(Entities.Video_Games.Where(w => w.Title.Contains(title)).ToList());
         
         public bool CheckIfGameExists(string gameTitle, string gameEdition, string gamePlatform)
             => (!(Entities.Video_Games.Any(game => game.Title == gameTitle && game.Platform == gamePlatform && game.Edition == gameEdition)));
